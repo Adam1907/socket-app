dfc5b4a2a792dd2e27ea9e82210a87ada26cf44c
unknown <adamhajdik@seznam.cz>
2021-03-20 13:29:05 +0100

    -Konecne funkcni chat

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
-const path = require('path');
-const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const http = require('http');
+const path = require('path');
+
 const app = express();
-
-
 const server = http.createServer(app);
 const io = socketio(server);
 
-
-// Static folder 'public'
+// Složka se statickými soubory
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-  console.log('User Online');
-  
-  socket.on('codeboard-message', (msg) => {
-  console.log('message: ' + msg);
-	socket.broadcast.emit('message-from-others', msg);
-  });
-  
-});
+    console.log("Novy uzivatel");
+    socket.on('chat', msg => {
+        io.emit('chat', msg);
+    });
+})
 
-const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => 
-console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, function() {
+    console.log(`Server nasloucha na portu ${PORT}`);
+})
